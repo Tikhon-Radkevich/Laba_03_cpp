f584be92defb26438ea4c34c09bca0312a422246
Tikhon <tikhon.radk@gmail.com>
2023-09-29 17:58:47 +0300

    fix idx; need to add del / fix threads;

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
-
+#include <random>
 
 using std::cout;
 using std::cin;
 using std::vector;
 
-
 int* array;
 int arraySize;
 
 HANDLE* markerThreads; // Array of marker thread handles
-HANDLE mainEvent; // Event for main thread to wait on
-HANDLE* markerEvents; // Array of marker threads to signal main thread
+HANDLE mainEvent; // Event for the main thread to wait on
+HANDLE* markerEvents; // Array of marker threads to signal the main thread
+HANDLE* StopMarkerEvents;
 
 struct MarkerThreadArgs {
     int markerId;
@@ -23,45 +23,45 @@ struct MarkerThreadArgs {
 
 // Marker thread
 DWORD WINAPI MarkerThread(LPVOID lpParam) {
-    MarkerThreadArgs args = *reinterpret_cast<MarkerThreadArgs*>(lpParam);
-    int markerId = args.markerId;
+    MarkerThreadArgs* t_args = reinterpret_cast<MarkerThreadArgs*>(lpParam);
+    int markerId = t_args->markerId;
+    cout << markerId << "\n";
 
-    srand(static_cast<unsigned int>(time(NULL)) + markerId);
+    std::random_device rd;
+    std::mt19937 rng(rd() + markerId);
+    std::uniform_int_distribution<int> dist(0, arraySize-1);
 
-    
 
     while (true) {
         // Wait for the main thread to signal start
         WaitForSingleObject(mainEvent, INFINITE);
 
-        cout << markerId << " MarkerThread started\n";
+        int indexToMark = dist(rng);
 
-        // Generate a random number
-        int randomNumber = rand();
-
-        // Calculate the index to mark
-        int indexToMark = randomNumber % arraySize;
-
-        // Sleep for 5 milliseconds
-        Sleep(5);
-
-        // Check if the element at the calculated index is zero
+        Sleep(3000);
         if (array[indexToMark] == 0) {
             // Mark the element with the markerId
             array[indexToMark] = markerId;
 
+            for (int i = 0; i < arraySize; i++)
+            {
+                cout << array[i] << " ";
+            }
+            cout << std::endl;
+
             // Sleep for another 5 milliseconds
             Sleep(5);
 
-            // Signal the main thread that marking is complete
-            SetEvent(markerEvents[markerId]);
+
         } else {
             // Notify the main thread that marking is impossible
-            SetEvent(markerEvents[markerId]);
-            WaitForSingleObject(mainEvent, INFINITE); // Wait for a response from the main thread
+            SetEvent(markerEvents[markerId-1]);
+            ResetEvent(StopMarkerEvents[markerId-1]);
+            cout << "\nSleep" << std::endl;
+            WaitForSingleObject(StopMarkerEvents[markerId-1], INFINITE);
+            cout << "StopMarkerEvents\n";
         }
     }
-
     return 0;
 }
 
@@ -80,16 +80,18 @@ int main() {
     // Create an array of marker thread handles and events
     markerThreads = new HANDLE[numMarkerThreads];
     markerEvents = new HANDLE[numMarkerThreads];
+    StopMarkerEvents = new HANDLE[numMarkerThreads];
 
     // Create the main event
     mainEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
     // Create marker threads
     for (int i = 0; i < numMarkerThreads; i++) {
-        MarkerThreadArgs args;
-        args.markerId = i;
+        MarkerThreadArgs* thread_args = new MarkerThreadArgs{i+1};
         markerEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
-        markerThreads[i] = CreateThread(NULL, 0, MarkerThread, &args, 0, NULL);
+        StopMarkerEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+        markerThreads[i] = CreateThread(NULL, 0, MarkerThread, thread_args, 0, NULL);
+        // cout << "Marker thread " << i << " created\n";
     }
 
     // Signal marker threads to start
@@ -100,7 +102,11 @@ int main() {
         DWORD waitResult = WaitForMultipleObjects(numMarkerThreads, markerEvents, TRUE, INFINITE);
 
         if (waitResult == WAIT_OBJECT_0) {
-            // All marker threads signaled completion
+            ResetEvent(mainEvent);
+
+            for (int i=0; i < numMarkerThreads; i ++){
+                SetEvent(StopMarkerEvents[i]);
+            }
 
             // Print the array contents
             cout << "Array contents: ";
@@ -109,25 +115,24 @@ int main() {
             }
             cout << std::endl;
 
+            cout << "All marker threads completed\n";
+
             // Prompt for a marker thread to terminate
             int markerId;
-            cout << "Enter the marker thread to terminate (0-" << numMarkerThreads - 1 << "): ";
+            cout << "Enter the marker thread to terminate (1-" << numMarkerThreads << "): ";
             cin >> markerId;
 
-            if (markerId >= 0 && markerId < numMarkerThreads) {
-                // Signal the selected marker thread to terminate
-                TerminateThread(markerThreads[markerId], 0);
+            TerminateThread(markerThreads[markerId-1], 0);    
 
-                // Reset the marker thread event
-                ResetEvent(markerEvents[markerId]);
-
-                // Clear the marked elements
-                for (int i = 0; i < arraySize; i++) {
-                    if (array[i] == markerId) {
-                        array[i] = 0;
-                    }
+            // Clear the marked elements
+            for (int i = 0; i < arraySize; i++) {
+                if (array[i] == markerId) {
+                    array[i] = 0;
                 }
             }
+            
+
+            SetEvent(mainEvent);
         }
     }
 
@@ -135,11 +140,13 @@ int main() {
     for (int i = 0; i < numMarkerThreads; i++) {
         CloseHandle(markerThreads[i]);
         CloseHandle(markerEvents[i]);
+        CloseHandle(StopMarkerEvents[i]);
     }
 
     delete[] array;
     delete[] markerThreads;
     delete[] markerEvents;
+    delete[] StopMarkerEvents;
 
     return 0;
 }
